1132c9ba4c82ebe435d0e68995740db80bb7f9ec
akowala <110314169+akowala@users.noreply.github.com>
2022-09-22 22:02:45 -0500

    reinstalled inquirer. added questions in index.js using the template from the mini project

--- script/index.js
+++ script/index.js
@@ -1,33 +1,43 @@
 const fs = require ("fs");
 const inquirer = require("inquirer");
+const createMD = require('./generateMD');
 
 inquirer
     .prompt([
     {
     type: "input",
-    message: "What is your name?",
+    message: "Please provide the name of your project.",
     name: "name"
 }, {
     type: "input",
-    message:"What city and state do you live in?",
-    name: "location"
+    message:"Please enter a desription of your project.",
+    name: "description"
 }, {
     type: "input",
-    message:"Tell me a little about yourself.",
-    name:"bio"
+    message:"Please include all information about installation instructions",
+    name:"installation"
 }, {
-    type:"linkedin",
-    message:"Please provide a link to your LinkedIn.",
-    name:"linkedinLink"
+    type:"input",
+    message:"Please include details about the usage of your project.",
+    name:"usage"
 }, {
-    type:"github",
-    message:"Please provide a link to your GitHub.",
-    name:"githubLink"
+    type:"input",
+    message:"Please include contribution guidelines.",
+    name:"contributions"
+}, {
+    type:"input",
+    message:"Please including testing instructions for your project.",
+    name: "testing"
+}, {
+    type: "list",
+    message:"Please choose from a list of licenses for your application.",
+    name: "license",
+    choices:[ "MIT", "Apache 2.0", "Mozilla Public, 2.0"]
 }
 ])
 
 .then((response)=>
-    fs.appendFile('index.html', JSON.stringify(response, null, 2), (err) => 
-        err ? console.log(err) : console.log("Success!")
+    fs.appendFile('README.md', createREADME(data),(err) => 
+        err ? console.log(err) : console.log("Look at your readme page!")
     )
     );
